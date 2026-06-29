0ff9853d74fc16b9197e7701f5603fb3082e5bb2
citra <chaitraursc.de@gmail.com>
2026-06-29 23:24:50 +0200

    feat: added various semantic and custom attributes under parent and child span

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from math import factorial
-from flask import Flask
+from flask import Flask, request
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.semconv.trace import SpanAttributes
 
 # Set up OpenTelemetry tracing and initialize the tracer provider
 tracer_provider = TracerProvider()
@@ -27,9 +28,22 @@ app = Flask(__name__)
 
 def demo():
 	# Start a new span named "demo_span" using the tracer
-	with tracer.start_as_current_span("demo_span"):
-		result = factorial(5)
-		with tracer.start_as_current_span("child_span"):
-			pass  # This is a placeholder for any additional logic you want to include in the child span
-		return str(f"The factorial of 5 is: {result}")
+	with tracer.start_as_current_span("demo_span") as parent:
+		# Set various semanticattributes for the parent span based on the incoming HTTP request
+		parent.set_attribute(SpanAttributes.HTTP_METHOD, request.method)
+		parent.set_attribute(SpanAttributes.HTTP_ROUTE, request.path)
+		parent.set_attribute(SpanAttributes.HTTP_URL, request.url)
+		parent.set_attribute(SpanAttributes.SERVER_ADDRESS, request.environ.get("SERVER_NAME"))
+		parent.set_attribute(SpanAttributes.SERVER_PORT, request.environ.get("SERVER_PORT"))
+		parent.set_attribute(SpanAttributes.CLIENT_ADDRESS, request.remote_addr)
+		parent.set_attribute(SpanAttributes.CLIENT_PORT, request.environ.get("REMOTE_PORT"))
+		
+		with tracer.start_as_current_span("child_span") as child:
+			result = factorial(5)
+			# Set attributes for the child span, custom:input value and the result, semantic: status code
+			child.set_attribute("factorial_input", 5)
+			child.set_attribute("factorial_result", result)
+			child.set_attribute(SpanAttributes.HTTP_STATUS_CODE, 200)
+
+			return str(f"The factorial of 5 is: {result}")
 	
